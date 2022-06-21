7bb5a2d743b5ed35bb2bbb151d065481028b3dc4
Leoni <leoni.murilo@hotmail.com>
2022-06-21 13:08:43 -0300

    fix(release): Fix semantic-release git plugin config

--- dist/esm/types/components/Footer/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-export interface FooterProps {
-    label: string;
-}
-declare const Footer: (props: FooterProps) => JSX.Element;
-export default Footer;
--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-interface FooterProps {
-    label: string;
-}
-declare const Footer: (props: FooterProps) => JSX.Element;
-
-export { Footer };
