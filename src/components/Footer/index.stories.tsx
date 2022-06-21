ad6304ea50ba5155f4626bf48ddf5279cc2b7646
Leoni <leoni.murilo@hotmail.com>
2022-06-21 09:35:00 -0300

    refactor(eslint): Add prettier & eslint

--- src/components/Footer/index.stories.tsx
+++ src/components/Footer/index.stories.tsx
@@ -1,26 +1,26 @@
-import React from "react";
-import { ComponentStory, ComponentMeta } from "@storybook/react";
-import Footer from "./index";
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import Footer from './index'
 
 // The default export defines where the button will appear in the Storybook
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "Components/Footer",
+  title: 'Components/Footer',
   component: Footer,
-} as ComponentMeta<typeof Footer>;
+} as ComponentMeta<typeof Footer>
 
 // Template determines which component is actually being rendered, and which default args/props to apply to it
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Footer> = (args) => <Footer {...args} />;
+const Template: ComponentStory<typeof Footer> = (args) => <Footer {...args} />
 
 // The Template.bind objects are instances or example states of the component
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-export const HelloWorld = Template.bind({});
+export const HelloWorld = Template.bind({})
 HelloWorld.args = {
-  label: "Hello Red Hat!",
-};
+  label: 'Hello Red Hat!',
+}
 
-export const ClickMe = Template.bind({});
+export const ClickMe = Template.bind({})
 ClickMe.args = {
-  label: "Other example",
-};
+  label: 'Other example',
+}
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import style from "./styles.module.scss";
+import React from 'react'
+import style from './styles.module.scss'
 
 export interface FooterProps {
-  label: string;
+  label: string
 }
 
-const Footer = (props: FooterProps) => {
-  return <div className={style.myTestClass}>
+const Footer = (props: FooterProps) => (
+  <div className={style.myTestClass}>
     <p>My test 1</p>
     <p>{props.label}</p>
-  </div>;
-};
+  </div>
+)
 
-export default Footer;
+export default Footer
