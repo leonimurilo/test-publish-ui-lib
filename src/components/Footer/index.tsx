a527834253b96c5375fc8bb2479608f6385c11f2
Leoni <leoni.murilo@hotmail.com>
2022-06-21 09:56:30 -0300

    fix(footer): Fix footer lint

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,14 +5,11 @@ export interface FooterProps {
   label: string;
 }
 
-const Footer = (props: FooterProps) => {
-  const x = 12222;
-  return (
-    <div className={style.myTestClass}>
-      <p>My test 1</p>
-      <p>{props.label}</p>
-    </div>
-  );
-};
+const Footer = (props: FooterProps) => (
+  <div className={style.myTestClass}>
+    <p>My test 1</p>
+    <p>{props.label}</p>
+  </div>
+);
 
 export default Footer;
